a031aa3de709c095a24b6f8a902d4b1bdd3e0caf
Ron Bershatsky <ron.b@spinomenal.com>
2025-02-02 14:21:17 +0200

    fix build dist dest

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import TextfitManager from './components/TextfitManager.vue';
 import TextfitLimiter from './components/TextfitLimiter.vue';
 
-export {
-    TextfitManager,
-    TextfitLimiter
-};
+export { TextfitManager, TextfitLimiter };
