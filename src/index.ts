e84dc8b7c7aa69ae411abb59e0b5458b27723834
Ron Bershatsky <ron.b@spinomenal.com>
2025-02-02 14:04:17 +0200

    small fix path alias

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import TextfitManager from '@/components/TextfitManager.vue';
-import TextfitLimiter from '@/components/TextfitLimiter.vue';
+import TextfitManager from './components/TextfitManager.vue';
+import TextfitLimiter from './components/TextfitLimiter.vue';
 
 export {
     TextfitManager,
