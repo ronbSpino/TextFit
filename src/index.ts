ab1802193d5b24095592a2b9b72e5f9e98d2480b
Ron Bershatsky <ron.b@spinomenal.com>
2025-02-03 16:29:58 +0200

    small fix alias import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import TextfitManager from './components/TextfitManager.vue';
-import TextfitLimiter from './components/TextfitLimiter.vue';
+import TextfitManager from '@/components/TextfitManager.vue';
+import TextfitLimiter from '@/components/TextfitLimiter.vue';
 
 export { TextfitManager, TextfitLimiter };
